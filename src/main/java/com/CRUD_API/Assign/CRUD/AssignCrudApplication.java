887f6841a61c42877fc9c3d4793569c8af1fe421
Abhishek <abhisam7007871@gmail.com>
2024-08-27 13:48:16 +0530

    Initial Commit

---
+++ src/main/java/com/CRUD_API/Assign/CRUD/AssignCrudApplication.java
@@ -0,0 +1,14 @@
+package com.CRUD_API.Assign.CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignCrudApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AssignCrudApplication.class, args);
+	}
+
+}
